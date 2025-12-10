c8c0019af0b01538c8438f4bfefdba0a1aaaf76a
AntikRay <antikray2006@gmail.com>
2025-12-10 19:22:15 +0530

    Create queue_using_stl.cpp

---
+++ Arrays/Strings/LinkedList/Stack/Queue/queue_using_stl.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    queue<int> q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    cout << "Front: " << q.front() << endl;
+    cout << "Back: " << q.back() << endl;
+
+    q.pop();
+
+    cout << "Front after pop: " << q.front() << endl;
+}
