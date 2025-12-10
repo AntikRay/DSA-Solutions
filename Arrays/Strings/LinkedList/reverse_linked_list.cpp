ea39a6b1834f17e4c08e13f223a7a65be2c5e843
AntikRay <antikray2006@gmail.com>
2025-12-10 18:35:44 +0530

    Create reverse_linked_list.cpp

---
+++ Arrays/Strings/LinkedList/reverse_linked_list.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int x){ data = x; next = NULL; }
+};
+
+Node* reverse(Node* head){
+    Node* prev = NULL;
+    Node* curr = head;
+
+    while(curr){
+        Node* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+int main(){
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+
+    head = reverse(head);
+
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
