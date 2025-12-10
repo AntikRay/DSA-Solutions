097b47a82a225782185d50f8af8ad844e6a21fdf
AntikRay <antikray2006@gmail.com>
2025-12-10 18:36:42 +0530

    Create middle_of_ll.cpp

---
+++ Arrays/Strings/LinkedList/middle_of_ll.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int x){ data = x; next = NULL; }
+};
+
+Node* middle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main(){
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    cout << middle(head)->data;
+}
