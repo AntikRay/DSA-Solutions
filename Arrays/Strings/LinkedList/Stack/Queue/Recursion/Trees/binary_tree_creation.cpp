7d4078ee49f6f0bd8a4fe3a7ca1d8e6c10548533
AntikRay <antikray2006@gmail.com>
2025-12-10 19:26:52 +0530

    Create binary_tree_creation.cpp

---
+++ Arrays/Strings/LinkedList/Stack/Queue/Recursion/Trees/binary_tree_creation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void preorder(Node* root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    cout << "Preorder: ";
+    preorder(root);
+}
