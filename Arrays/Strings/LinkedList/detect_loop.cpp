31a44c02b54665125d939a787c66e81bd43b5187
AntikRay <antikray2006@gmail.com>
2025-12-10 18:36:14 +0530

    Add loop detection in linked list
    Implement Floyd's Cycle Detection Algorithm to check for loops in a linked list.

---
+++ Arrays/Strings/LinkedList/detect_loop.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int x){ data = x; next = NULL; }
+};
+
+bool detectLoop(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
+
+int main(){
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = head; // loop
+
+    cout << detectLoop(head);
+}
